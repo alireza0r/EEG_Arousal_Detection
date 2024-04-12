09192330a9ea05d5c9101dbec4502303af80e8f0
AlirezaRahmati <35702886+alireza0r@users.noreply.github.com>
2024-04-12 17:33:34 +0330

    Create cluster.py

---
+++ Models/cluster.py
@@ -0,0 +1,35 @@
+from sklearn.cluster import DBSCAN
+import numpy as np
+
+def train_
+
+class DBSCAN:
+  def __init__(self, eps=1, min_samples=5):
+    self.eps = eps
+    self.min_samples = min_samples
+    
+    self.cluster = None
+
+  def train(self, x):
+    self.cluster = DBSCAN(eps=self.eps, min_samples=self.min_samples)
+    self.cluster.fit(x)
+    return self.cluster
+
+  def save(self, path):
+    # Save the DBSCAN clustering results 
+    with open(path+'.pkl', 'wb') as f:
+      pickle.dump(self.cluster, f)
+
+  def load(self, path):
+    # Save the DBSCAN clustering results 
+    with open(path, 'rb') as f:
+      self.cluster = loaded_dbscan = pickle.load(f)
+
+  def __call__(self, x):
+    return self.cluster.fit_predict(x)
+
+if __name__ == '__main__':
+  dcscan = DBSCAN()
+  dcscan.train(x)          # Train
+  result = dcscan(x_test)  # Test
+  print(result)
