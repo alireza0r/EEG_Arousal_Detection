b32e1b8192aac024597ef4380508b39b2c3b0755
AlirezaRahmati <35702886+alireza0r@users.noreply.github.com>
2024-04-30 18:22:52 +0330

    Update layers.py

--- Libs/layers.py
+++ Libs/layers.py
@@ -2,19 +2,15 @@ import torch
 import torch.nn as nn
 
 class AvgGlobalPool2d(nn.Module):
-  def __init__(self, axis):
+  def __init__(self,):
     super().__init__()
-  
-    self.axis = axis
 
   def forward(self, x):
     return torch.mean(x, dim=[x.size(-2), x.size(-1)])
 
 class MaxGlobalPool2d(nn.Module):
-  def __init__(self, axis):
+  def __init__(self):
     super().__init__()
-  
-    self.axis = axis
 
   def forward(self, x):
     return torch.max(x, dim=[x.size(-2), x.size(-1)])
